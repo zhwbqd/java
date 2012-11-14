928399b9701356c4513be804c3309734f81a9401
zhwbqd <zhwbqd@gmail.com>
2012-11-14 12:53:29 +0800

    add batch

---
+++ test/src/com/util/Global.java
@@ -0,0 +1,17 @@
+package com.util;
+
+/**
+ * @author <a href="mailto:liu.anxin13@gmail.com">Tony</a>
+ */
+public interface Global {
+	
+	/** 数据量 */
+	long NUM = 1000000;
+	
+	/** 是否批量 */
+	boolean IS_BATCH = true;
+	
+	/** 一次的批量数 */
+	int BATCH_NUMBER = 50;
+	
+}
