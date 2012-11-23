6650ae6f3977efcb8cb6a7e2f701c09b712ff931
zhwbqd <zhwbqd@gmail.com>
2012-11-23 14:33:22 +0800

    完成二进制输出补全0算法

--- JTR/src/zhwb/test/reflection/TestMethod.java
+++ JTR/src/zhwb/test/reflection/TestMethod.java
@@ -2,11 +2,10 @@ package zhwb.test.reflection;
 
 public class TestMethod {
 	public void retrieveVoidByIntegerByStringByStringByStringByString(
-			@ParamName(value = "p1") Integer p1,
-			@ParamName(value = "p2") String p2,
-			@ParamName(value = "p3") String p3,
-			@ParamName(value = "p4") String p4,
-			@ParamName(value = "p5") String p5) {
+@ParamName(value = "Country") Integer country,
+            @ParamName(value = "State") String state, @ParamName(value = "Postal") String postal,
+            @ParamName(value = "City") String city, @ParamName(value = "Name") String name)
+    {
 
 	}
 }
--- JTR/src/zhwb/test/reflection/TestReflection.java
+++ JTR/src/zhwb/test/reflection/TestReflection.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class TestReflection {
 
@@ -14,7 +15,7 @@ public class TestReflection {
 		Map<String, Map<Integer, String>> totalMap = new HashMap<String, Map<Integer, String>>();
 		Method[] methods = TestMethod.class.getDeclaredMethods();
 		for (Method method : methods) {
-			Map<Integer, String> map = new HashMap<Integer, String>();
+            Map<Integer, String> map = new TreeMap<Integer, String>();
 
 			int paramCount = method.getParameterTypes().length;
 
@@ -31,13 +32,36 @@ public class TestReflection {
 				}
 			}
 			
-			StringBuilder sb = new StringBuilder();
-			sb.append("test");
-			
 			for (int i = 0; i < totalNums; i++) {
-
+                StringBuilder sb = new StringBuilder();
+                sb.append("test");
+                String formatBinary = format(paramCount, Integer.toBinaryString(i));
+                sb.append(generateName(formatBinary, list));
+                map.put(i, sb.toString());
 			}
 			totalMap.put(method.getName(), map);
 		}
+        System.out.println(totalMap);
 	}
+
+    private static String format(int paramCount, String binaryString)
+    {
+        String temp = new String(binaryString);
+        for (int i = binaryString.length(); i < paramCount; i++)
+        {
+            temp = "0" + temp;
+        }
+        return temp;
+    }
+
+    private static String generateName(String binaryString, List<String> list)
+    {
+        StringBuilder sb = new StringBuilder();
+        char[] binary = binaryString.toCharArray();
+        for (int i = 0; i < binary.length; i++)
+        {
+            sb.append(list.get(i) + binary[i]);
+        }
+        return sb.toString().replaceAll(String.valueOf(0), "Null").replaceAll(String.valueOf(1), "NotNull");
+    }
 }
