d4dca2a226ad9ec774420e9c69bb8c3c0b8aed18
zhwbqd <zhwbqd@gmail.com>
2012-11-23 10:23:26 +0800

    test

--- xlts/src/json/test/TestGson.java
+++ xlts/src/json/test/TestGson.java
@@ -1,5 +1,7 @@
 package json.test;
 
+import java.util.Date;
+
 import com.google.gson.Gson;
 
 public class TestGson {
@@ -9,10 +11,9 @@ public class TestGson {
 	 */
 	public static void main(String[] args) {
 		Gson gs=new Gson();
-		gs.toJson("a:"+true);
-		gs.toJson("b:"+1);
-		
-		System.out.println(gs);
+        System.out.println(gs.toJson(String.valueOf("a")));
+        System.out.println(gs.toJson(Integer.valueOf(1)));
+        System.out.println(gs.toJson(new Date()));
 	}
 
 }
