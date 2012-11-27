32139f1a4ec02ceb9391254feaceae273b9aff3b
Jack <zhwbqd@gmail.com>
2012-11-27 22:21:08 +0800

    update the spring mvc 3

--- HelloWorldSpring3/src/main/java/zhwb/study/controller/HelloWorldController.java
+++ HelloWorldSpring3/src/main/java/zhwb/study/controller/HelloWorldController.java
@@ -1,9 +1,3 @@
-/*
- * Copyright Notice ====================================================
- * This file contains proprietary information of Hewlett-Packard Co.
- * Copying or reproduction without prior written approval is prohibited.
- * Copyright (c) 2009   All rights reserved. ======================
- */
 
 package zhwb.study.controller;
 
@@ -14,10 +8,10 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController
 {
-    @RequestMapping("/helloworld")
-    public ModelAndView helloWorld()
+	@RequestMapping("/hellojack")
+	public ModelAndView helloWorl()
     {
         String message = "Hello World, Spring 3.0!";
-        return new ModelAndView("helloworld", "message", message);
+		return new ModelAndView("helloworld", "message", message);
     }
 }
