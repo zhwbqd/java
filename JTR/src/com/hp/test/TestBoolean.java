519f76e926738e6cc317adddb17a7fdfff227b91
zhwbqd <zhwbqd@gmail.com>
2012-11-27 17:57:20 +0800

    spring MVC

---
+++ JTR/src/com/hp/test/TestBoolean.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright Notice ====================================================
+ * This file contains proprietary information of Hewlett-Packard Co.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 2009   All rights reserved. ======================
+ */
+
+package com.hp.test;
+
+public class TestBoolean
+{
+    public static void main(String[] args)
+    {
+        System.out.println(new Boolean("d"));
+    }
+}
