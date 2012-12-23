1f2e446aa80b7881d49f451822156713533b1017
Jack <zhwbqd@gmail.com>
2012-12-23 20:25:23 +0800

    Decorate Design pattern

---
+++ design-pattern-study/src/main/java/zhwb/decorate/Manager.java
@@ -0,0 +1,39 @@
+package zhwb.decorate;
+
+abstract class Manager implements Project {
+    private Project project;        //实际上存放的是代码工人对象 
+
+    public Manager(Project project) { 
+        this.project = project; 
+    } 
+
+    /** 
+     * 编码 
+     */ 
+    public void doCoding() { 
+        //项目经理开始新的工作 
+        startNewWork(); 
+    } 
+
+    /** 
+     * 模板：定义项目经理自己的事情 
+     */ 
+    public void startNewWork() { 
+        //项目经理在做早期工作 
+        doEarlyWork(); 
+        //项目经理很牛，做完需求和设计后，直接将编码委派给代码工人干 
+        project.doCoding(); 
+        //项目经理在做收尾工作 
+        doEndWork(); 
+    } 
+
+    /** 
+     * 项目经理自己的事情：做早期工作 
+     */ 
+	abstract void doEarlyWork();
+
+    /** 
+     * 项目经理做收尾工作 
+     */ 
+	abstract void doEndWork();
+}
