0ab191cfe40f621cfa2ffdbb849f0d5291a93d05
zhwbqd <zhwbqd@gmail.com>
2013-01-11 18:11:57 +0800

    Refactor the email sender

--- mailSender/src/test/java/zhwb/javamail/bean/EmailSenderTest.java
+++ mailSender/src/test/java/zhwb/javamail/bean/EmailSenderTest.java
@@ -8,8 +8,8 @@
 package zhwb.javamail.bean;
 
 import java.io.IOException;
-
-import javax.mail.MessagingException;
+import java.util.HashMap;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
@@ -19,16 +19,24 @@ public class EmailSenderTest extends TestCase
 {
     @Test
     public void testSendEmail()
-        throws IOException, MessagingException
+        throws IOException
     {
-        String mailTo = "wen-bin.zhang@hp.com";
-        String mailFrom = "wen-bin.zhang@hp.com";
-        String smtpHost = "smtp.hp.com";
+        String mailTo = "wen-bin.zhang@hp.com, zhwbqd@unkonwnsdadggggggaas.dom";
+        String mailFrom = "sbs-shanghai-dev@hp.com";
+        String smtpHost = "smtp3.hp.com";
         String subject = "TEST";
         String msgContent = "TEST";
         String port = "25";
         Email email = new Email.Builder(mailTo, mailFrom, smtpHost, subject, msgContent, port).builder();
-        EmailSender bean = new EmailSender(email);
-        bean.sendEmail();
+        EmailSender bean = new EmailSender(email, true);
+        Map<String, String> errorMap = new HashMap<String, String>();
+        try
+        {
+            bean.sendEmail();
+        }
+        catch (Exception e)
+        {
+            e.getMessage();
+        }
     }
 }
