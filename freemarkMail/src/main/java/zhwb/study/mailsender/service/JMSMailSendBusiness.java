68543eb74e4c90baea14ab6e5e9d53f53486124c
zhwbqd <zhwbqd@gmail.com>
2013-02-08 00:29:01 +0800

    Online mail function

--- freemarkMail/src/main/java/zhwb/study/mailsender/service/JMSMailSendBusiness.java
+++ freemarkMail/src/main/java/zhwb/study/mailsender/service/JMSMailSendBusiness.java
@@ -1,24 +1,33 @@
 package zhwb.study.mailsender.service;
 
+import java.util.concurrent.Callable;
+
 import zhwb.study.mailsender.bean.MailSenderInfo;
+import zhwb.study.mailsender.bean.ResponseStatus;
 import zhwb.study.mailsender.bean.ResponseStatusCode;
 import zhwb.study.mailsender.jms.EmailJmsPublisher;
+import zhwb.study.mailsender.service.mail.IEmailService;
 import zhwb.study.mailsender.service.template.ITemplateService;
+import zhwb.study.mailsender.utils.ThreadPoolManager;
 
 public class JMSMailSendBusiness
 {
+    private IEmailService emailService;
+
     private EmailJmsPublisher jmsPublisher;
 
     private ITemplateService templateService;
 
-    public JMSMailSendBusiness(final EmailJmsPublisher jmsPublisher, final ITemplateService templateService)
+    public JMSMailSendBusiness(final IEmailService emailService, final EmailJmsPublisher jmsPublisher,
+            final ITemplateService templateService)
     {
         super();
+        this.emailService = emailService;
         this.jmsPublisher = jmsPublisher;
         this.templateService = templateService;
     }
 
-    public ResponseStatusCode dispatchEmailByMailSenderInfo(final MailSenderInfo info)
+    public ResponseStatusCode dispatchEmailByMailSenderInfo(final MailSenderInfo info, final boolean isOnline)
     {
         ResponseStatusCode status = new ResponseStatusCode();
         if (info == null)
@@ -35,10 +44,18 @@ public class JMSMailSendBusiness
             status.setStatusCode(ResponseStatusCode.EMAIL_TEMPLATE_PROCESS_FAILED);
             return status;
         }
+        boolean success = false;
+        if (isOnline)
+        {
+            Callable<ResponseStatus> task = emailService.createSendTask(emailText, info.getSendAddress(), info.isSendGroup(),
+                    info.getFromAddress(), info.getSubject());
 
-        /*unchecked JmsException will throw here*/
-        boolean success = jmsPublisher.sendEmailJms(emailText, info);
-
+            success = ThreadPoolManager.getInstance().submitTask(task, true);
+        }
+        else
+        {/*unchecked JmsException will throw here*/
+            success = jmsPublisher.sendEmailJms(emailText, info);
+        }
         if (success)
         {
             status.setStatusCode(ResponseStatusCode.SUCCESS);
