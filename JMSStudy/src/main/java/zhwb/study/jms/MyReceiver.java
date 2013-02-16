527212f70222bfe736a59b5120941e851f16fdb2
Jack <zhwbqd@gmail.com>
2013-02-16 09:48:14 +0800

    merge

--- JMSStudy/src/main/java/zhwb/study/jms/MyReceiver.java
+++ JMSStudy/src/main/java/zhwb/study/jms/MyReceiver.java
@@ -1,9 +1,3 @@
-/*
- * Copyright Notice ====================================================
- * This file contains proprietary information of Hewlett-Packard Co.
- * Copying or reproduction without prior written approval is prohibited.
- * Copyright (c) 2012 All rights reserved. =============================
- */
 
 package zhwb.study.jms;
 
