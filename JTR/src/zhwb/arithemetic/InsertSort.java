e6d13b51b8cf62de055bb263d95ec5efbaf47249
Jack <zhwbqd@gmail.com>
2013-02-16 10:32:21 +0800

    insertSort

---
+++ JTR/src/zhwb/arithemetic/InsertSort.java
@@ -0,0 +1,22 @@
+package zhwb.arithemetic;
+
+import java.util.Arrays;
+
+public class InsertSort {
+	public static void main(String[] args) {
+		Long[] unsort = { 22L, 88L, 99L, 77L, 66L, 55L, 44L, 33L, 11L };
+
+		for (int out = 1; out < unsort.length; out++) {
+			int loop = out;
+			long temp = unsort[out];
+			while (loop > 0 && unsort[loop - 1] >= temp) {
+				unsort[loop] = unsort[loop - 1];
+				--loop;
+			}
+			unsort[loop] = temp;
+		}
+
+		System.out.println(Arrays.asList(unsort));
+
+	}
+}
