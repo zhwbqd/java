ecc325f2fb0ab6743af8d16aa4ab29de73cb018d
Jack <zhwbqd@gmail.com>
2013-03-17 18:06:01 +0800

    Recursion

---
+++ arithemetic/src/main/java/arithemtic/recursion/TriangleApp.java
@@ -0,0 +1,38 @@
+package arithemtic.recursion;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class TriangleApp
+   {
+   static int theNumber;
+
+   public static void main(String[] args) throws IOException
+      {
+      System.out.print("Enter a number: ");
+      theNumber = getInt();
+      int theAnswer = triangle(theNumber);
+      System.out.println("Triangle="+theAnswer);
+      }  // end main()
+//-------------------------------------------------------------
+   public static int triangle(int n)
+      {
+      if(n==1)
+         return 1;
+      else
+         return  ( n + triangle(n-1) );
+      }
+//-------------------------------------------------------------
+   public static String getString() throws IOException
+      {
+      InputStreamReader isr = new InputStreamReader(System.in);
+      BufferedReader br = new BufferedReader(isr);
+      String s = br.readLine();
+      return s;
+      }
+//-------------------------------------------------------------
+   public static int getInt() throws IOException
+      {
+      String s = getString();
+      return Integer.parseInt(s);
+      }
+}
