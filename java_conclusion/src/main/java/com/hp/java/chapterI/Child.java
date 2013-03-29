4883c6fe20a7604a08b32225f95c5e9c4da4d449
zhwbqd <zhwbqd@gmail.com>
2013-03-29 16:43:50 +0800

    add java mail

---
+++ java_conclusion/src/main/java/com/hp/java/chapterI/Child.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright Notice ====================================================
+ * This file contains proprietary information of Hewlett-Packard Co.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 2012 All rights reserved. =============================
+ */
+
+package com.hp.java.chapterI;
+
+public class Child extends Parent
+{
+
+}
