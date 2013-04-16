cfc57439aa5656127fcbe69a5ac2ab937e2b8191
zhwbqd <zhwbqd@gmail.com>
2013-04-16 15:04:16 +0800

    Date conver UTC

--- java_conclusion/src/main/java/jtr/date/DateCastTest.java
+++ java_conclusion/src/main/java/jtr/date/DateCastTest.java
@@ -1,57 +1,50 @@
-/*
- * Copyright Notice ====================================================
- * This file contains proprietary information of Hewlett-Packard Co.
- * Copying or reproduction without prior written approval is prohibited.
- * Copyright (c) 2009   All rights reserved. ======================
- */
 
 package jtr.date;
 
-import java.sql.Timestamp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.SimpleTimeZone;
-
-import org.junit.Test;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class DateCastTest
 {
+    private static final String DATEFORMAT = "yyyy-MM-dd HH:mm:ss";
 
-	public static void main(String[] args) {
-		Map map = new HashMap();
-		map.put("123", "4");
-		map.put("123", "5");
-
-		for (Object entry : map.entrySet()) {
-			System.out.println(entry.toString());
-		}
+	public static void main(final String[] args) {
+        System.out.println(new Date().toString());
+        System.out.println(GetUTCdatetimeAsString());
 	}
 
-    @Test
-    public void classCast()
+    public static Date GetUTCdatetimeAsDate()
     {
-        try
-        {
-            String st = "2005-12-02 12:00:00";
-            Timestamp starttime = Timestamp.valueOf(st);
-            System.out.println(starttime.toString());
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
-
+        //note: doesn't check for null
+        return StringDateToDate(GetUTCdatetimeAsString());
     }
 
-    @Test
-    public void fmsExcep()
+    public static String GetUTCdatetimeAsString()
     {
-        SimpleDateFormat sdf = new SimpleDateFormat();
-        sdf.setTimeZone(new SimpleTimeZone(0, "GMT"));
-        sdf.applyPattern("dd MMM yyyy HH:mm:ss z");
-        java.sql.Date date = null;
-        System.out.println(sdf.format(((java.util.Date)date)));
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATEFORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        final String utcTime = sdf.format(new Date());
+
+        return utcTime;
+    }
+
+    public static Date StringDateToDate(final String StrDate)
+    {
+        Date dateToReturn = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATEFORMAT);
+
+        try
+        {
+            dateToReturn = dateFormat.parse(StrDate);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+
+        return dateToReturn;
     }
 
 }
