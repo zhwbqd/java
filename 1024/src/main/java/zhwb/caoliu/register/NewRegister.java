d760c9bf0e87fc52a12d25eaf5823d8467fc2322
Jack <zhwbqd@gmail.com>
2013-04-20 01:36:16 +0800

    new arithemetic of register

---
+++ 1024/src/main/java/zhwb/caoliu/register/NewRegister.java
@@ -0,0 +1,91 @@
+package zhwb.caoliu.register;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class NewRegister {
+	private static final String PATTERN = "[a-f0-9*]{16}";
+	private static final char PLACE_HOLDER = '*';
+	private static final char[] WORDS = new char[] { 'a', 'b', 'c', 'd', 'e',
+			'f' };
+	private static final char[] NUMBERS = new char[] { '0', '1', '2', '3', '4',
+			'5', '6', '7', '8', '9' };
+
+	public static void main(String[] args) {
+
+		List<String> list2 = getResultFromInput("abc*de*f", Type.MIX); // 100+36+120
+		int count = 0;
+		for (String string : list2) {
+			System.out.println(string);
+			count++;
+		}
+		System.out.println(count);
+	}
+
+	public static List<String> getResultFromInput(String code, Type type) {
+		Deque<String> queue = new LinkedList<String>();
+		queue.addLast(code);
+		switch (type) {
+		case CHAR:
+			while (queue.peekFirst() != null) {
+				String string = queue.peekFirst();
+				int place = string.indexOf(PLACE_HOLDER);
+				if (place >= 0) {
+					queue.pollFirst();
+					for (char replace : WORDS) {
+						char[] array = string.toCharArray();
+						array[place] = replace;
+						queue.addLast(String.valueOf(array));
+					}
+				} else {
+					break;
+				}
+			}
+			break;
+		case NUMBER:
+			while (queue.peekFirst() != null) {
+				String string = queue.peekFirst();
+				int place = string.indexOf(PLACE_HOLDER);
+				if (place >= 0) {
+					queue.pollFirst();
+					for (char replace : NUMBERS) {
+						char[] array = string.toCharArray();
+						array[place] = replace;
+						queue.addLast(String.valueOf(array));
+					}
+				} else {
+					break;
+				}
+			}
+			break;
+		case MIX:
+			while (queue.peekFirst() != null) {
+				String string = queue.peekFirst();
+				int place = string.indexOf(PLACE_HOLDER);
+				if (place >= 0) {
+					queue.pollFirst();
+					for (char replace : NUMBERS) {
+						char[] array = string.toCharArray();
+						array[place] = replace;
+						queue.addLast(String.valueOf(array));
+					}
+					for (char replace : WORDS) {
+						char[] array = string.toCharArray();
+						array[place] = replace;
+						queue.addLast(String.valueOf(array));
+					}
+				} else {
+					break;
+				}
+			}
+			break;
+		}
+
+		return new ArrayList<String>(new LinkedHashSet<String>(queue));
+
+	}
+
+}
