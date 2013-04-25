83cb6701193c198bd8b2debf22ce613d7d70b881
zhwbqd <zhwbqd@gmail.com>
2013-04-25 19:34:32 +0800

    DateFormat
    Change-Id: I5aedce2205cbdb893dafab4447c9f48624a4605d
    Signed-off-by: zhwbqd <zhwbqd@gmail.com>

--- java_conclusion/src/main/java/zhwb/study/date/CalenderTest.java
+++ java_conclusion/src/main/java/zhwb/study/date/CalenderTest.java
@@ -7,8 +7,13 @@
 
 package zhwb.study.date;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class CalenderTest
 {
@@ -17,9 +22,13 @@ public class CalenderTest
      * Description goes here.
      *
      * @param args
+     * @throws InterruptedException 
+     * @throws ParseException 
      */
     public static void main(final String[] args)
+        throws InterruptedException, ParseException
     {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         Calendar birth = Calendar.getInstance();
         birth.set(1989, 2, 2);
         Date date = birth.getTime();
@@ -32,13 +41,25 @@ public class CalenderTest
         now.add(Calendar.DAY_OF_YEAR, 100);
         System.out.println(now.getTime());
 
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.clear(Calendar.HOUR);
-        cal.clear(Calendar.MINUTE);
-        cal.clear(Calendar.SECOND);
-        System.out.println(cal.getTime());
+        /*compare date with different type */
+        Date d1 = new Date();
+        Thread.currentThread().sleep(300);
+        Date d2 = new Date();
+        System.out.println(d1.equals(d2));
 
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String s1 = sdf.format(d1);
+        String s2 = sdf.format(d2);
+        System.out.println(s1 + ", " + s2);
+        System.out.println(s1.equals(s2));
+
+        Calendar now1 = Calendar.getInstance();
+        now1.set(2013, 3, 25);
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.CHINA);
+        String serviceEndDate = df.format(now1.getTime());
+        System.out.println(Locale.getDefault());
+
+        System.out.println(df.format(new Date()));
     }
 
 }
