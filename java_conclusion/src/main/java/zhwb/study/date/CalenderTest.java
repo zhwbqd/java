4c033cc88dc7d3713b5ba13d6a25ca826399ae7a
zhwbqd <zhwbqd@gmail.com>
2013-04-25 12:06:33 +0800

    DateFormat

--- java_conclusion/src/main/java/zhwb/study/date/CalenderTest.java
+++ java_conclusion/src/main/java/zhwb/study/date/CalenderTest.java
@@ -7,8 +7,12 @@
 
 package zhwb.study.date;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class CalenderTest
 {
@@ -17,9 +21,13 @@ public class CalenderTest
      * Description goes here.
      *
      * @param args
+     * @throws InterruptedException 
+     * @throws ParseException 
      */
     public static void main(final String[] args)
+        throws InterruptedException, ParseException
     {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         Calendar birth = Calendar.getInstance();
         birth.set(1989, 2, 2);
         Date date = birth.getTime();
@@ -32,12 +40,25 @@ public class CalenderTest
         now.add(Calendar.DAY_OF_YEAR, 100);
         System.out.println(now.getTime());
 
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.clear(Calendar.HOUR);
-        cal.clear(Calendar.MINUTE);
-        cal.clear(Calendar.SECOND);
-        System.out.println(cal.getTime());
+        /*compare date with different type */
+        Date d1 = new Date();
+        Thread.currentThread().sleep(300);
+        Date d2 = new Date();
+        System.out.println(d1.equals(d2));
+
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String s1 = sdf.format(d1);
+        String s2 = sdf.format(d2);
+        System.out.println(s1 + ", " + s2);
+        System.out.println(s1.equals(s2));
+
+        String sd1 = sdf.format(new Date());
+        String sd2 = sdf.format(new Date());
+        System.out.println(sdf.parse(sd1));
+        System.out.println(sdf.parse(sd2));
+        System.out.println(sdf.parse(sd1).after(sdf.parse(sd2)));
+        System.out.println(sdf.parse(sd1).before(sdf.parse(sd2)));
+        System.out.println(sdf.parse(sd1).equals(sdf.parse(sd2)));
 
     }
 
