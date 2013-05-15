5c32fcc418d423222e4e933aa127f06718d407b2
zhwbqd <zhwbqd@gmail.com>
2013-05-15 13:45:37 +0800

    update sdf case

--- java_conclusion/src/main/java/zhwb/study/date/SimpleDateFormatIssue.java
+++ java_conclusion/src/main/java/zhwb/study/date/SimpleDateFormatIssue.java
@@ -12,6 +12,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class SimpleDateFormatIssue
 {
@@ -20,10 +21,21 @@ public class SimpleDateFormatIssue
     {
         //        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         String newtime = "Tue Mar 08 09:27:28 +0000 2011";
-        DateFormat df = new SimpleDateFormat("EEE MMM dd H:mm:ss Z yyyy", Locale.US);
+        DateFormat df = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.US);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
         Date sdate = df.parse(newtime);
         System.out.println(sdate);
+        df = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.CHINA);
+        System.out.println(df.format(sdate));
+
         DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.US);
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
         System.out.println(formatter.format(sdate));
+
+        String format = "yyyy-MM-dd HH:mm:ss";
+        df = new SimpleDateFormat(format, Locale.US);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        sdate = df.parse("2013-04-25 21:19:42.999999999999999999999");
+        System.out.println(sdate);
     }
 }
