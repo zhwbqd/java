5860b9d5ee3dba93a5df8c4ec5646307f1c70e96
zhwbqd <zhwbqd@gmail.com>
2013-05-30 11:35:53 +0800

    Select for update

---
+++ java_conclusion/src/main/java/zhwb/study/jdbc/UpdateDAO.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright Notice ====================================================
+ * This file contains proprietary information of Hewlett-Packard Co.
+ * Copying or reproduction without prior written approval is prohibited.
+ * Copyright (c) 2012 All rights reserved. =============================
+ */
+
+package zhwb.study.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UpdateDAO
+{
+    public ResultSet selectForUpdate()
+        throws Exception
+    {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection dataSource = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "password");
+        dataSource.setAutoCommit(false);
+        Statement statement = dataSource.createStatement();
+        boolean result = statement.execute("select * from test where status ='new' for update");
+        System.out.println(result);
+        Thread.sleep(10000);
+        dataSource.commit();
+        return statement.getResultSet();
+    }
+}
