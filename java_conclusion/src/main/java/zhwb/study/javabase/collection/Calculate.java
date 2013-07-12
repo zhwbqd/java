c6c7f31fa019c9de59e678e4e0bcee65063be622
zhwbqd <zhwbqd@gmail.com>
2013-07-12 17:30:53 +0800

    lucene

--- java_conclusion/src/main/java/zhwb/study/javabase/collection/Calculate.java
+++ java_conclusion/src/main/java/zhwb/study/javabase/collection/Calculate.java
@@ -1,11 +1,50 @@
 package zhwb.study.javabase.collection;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Calculate {
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		BigInteger big = new BigInteger("2");
 		BigInteger result = big.pow(100);
 		System.out.println(result);
+		
+		List<Person> plis = new ArrayList<Person>();
+        Person p = new Person();
+        p.age = 18;
+        plis.add(p);
+
+        Map<String, Person> map = new HashMap<String, Person>();
+        for (Person person : plis)
+        {
+            map.put("1", person);
+        }
+
+        for (Entry<String, Person> person : map.entrySet())
+        {
+            person.getValue().age = 20;
+            person.getValue().name = "Jack";
+        }
+
+        for (Person person : plis)
+        {
+            System.out.println(person.age);
+            System.out.println(person.name);
+        }
+    }
+
+    static class Person
+    {
+        public Person()
+        {
+        }
+
+        String name;
+
+        int age;
+    }
 	}
-}
--- java_conclusion/src/main/java/zhwb/study/javabase/collection/DeepCopyAndLightCopy.java
+++ java_conclusion/src/main/java/zhwb/study/javabase/collection/DeepCopyAndLightCopy.java
@@ -8,9 +8,10 @@
 package zhwb.study.javabase.collection;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class DeepCopyAndLightCopy
 {
@@ -23,22 +24,56 @@ public class DeepCopyAndLightCopy
     public static void main(final String[] args)
     {
 
-        List<Object> source = new ArrayList<Object>();
+        //        List<Object> source = new ArrayList<Object>();
+        //
+        //        source.add(new Object());
+        //        source.add(new Object());
+        //
+        //        // light copy???
+        //        List<Object> copy1 = new ArrayList<Object>(source);
+        //        source.add(new Object());
+        //        source.set(0, new Object());
+        //        System.out.println(copy1 + "  " + source);
+        //
+        //        // deep copy???
+        //        List<Object> copy2 = new ArrayList<Object>(Arrays.asList(new Object[source.size()]));
+        //        Collections.copy(copy2, source);
+        //        source.add(new Object());
+        //        System.out.println(copy2 + "  " + source);
 
-        source.add(new Object());
-        source.add(new Object());
+        List<Person> plis = new ArrayList<Person>();
+        Person p = new Person();
+        p.age = 18;
+        plis.add(p);
 
-        // light copy???
-        List<Object> copy1 = new ArrayList<Object>(source);
-        source.add(new Object());
-        source.set(0, new Object());
-        System.out.println(copy1 + "  " + source);
+        Map<String, Person> map = new HashMap<String, Person>();
+        for (Person person : plis)
+        {
+            map.put("1", person);
+        }
 
-        // deep copy???
-        List<Object> copy2 = new ArrayList<Object>(Arrays.asList(new Object[source.size()]));
-        Collections.copy(copy2, source);
-        source.add(new Object());
-        System.out.println(copy2 + "  " + source);
+        for (Entry<String, Person> person : map.entrySet())
+        {
+            person.getValue().age = 20;
+            person.getValue().name = "Jack";
+        }
+
+        for (Person person : plis)
+        {
+            System.out.println(person.age);
+            System.out.println(person.name);
+        }
     }
 
+    static class Person
+    {
+        public Person()
+        {
+        }
+
+        String name;
+
+        int age;
+    }
 }
+
