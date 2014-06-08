0d3156d5cb7d3edb9c41d4fd5339cbc41d9313b8
zhwbqd <zhwbqd@gmail.com>
2014-06-08 23:38:05 +0800

    classloader

---
+++ java_conclusion/src/main/java/zhwb/study/classloader/ClassLoaderTest.java
@@ -0,0 +1,20 @@
+package zhwb.study.classloader;
+
+/**
+ * Date: 14-6-8
+ * Time: 下午9:58
+ *
+ * @author jack.zhang
+ */
+public class ClassLoaderTest{
+    public static void main(String[] args) {
+        System.out.println(ClassLoaderTest.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+        System.out.println(ClassLoaderTest.class.getResource("").getPath());//同包
+        System.out.println(ClassLoaderTest.class.getResource("/").getPath());//classpath
+        System.out.println(ClassLoaderTest.class.getClassLoader().getResource("").getPath());//classpath
+        System.out.println(ClassLoaderTest.class.getClassLoader().getResource("/"));//null
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(Thread.currentThread().getContextClassLoader().getResource("").getPath());
+        System.out.println(Thread.currentThread().getContextClassLoader().getResource("test.props"));
+    }
+}
