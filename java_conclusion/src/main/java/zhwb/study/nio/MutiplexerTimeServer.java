d88758637eae694ed2d3a5edb6649b7cc4f84ff7
zhwbqd <zhwbqd@gmail.com>
2014-07-01 23:51:31 +0800

    fix pom

---
+++ java_conclusion/src/main/java/zhwb/study/nio/MutiplexerTimeServer.java
@@ -0,0 +1,117 @@
+package zhwb.study.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Date: 14-6-30
+ * Time: 下午9:53
+ *
+ * @author jack.zhang
+ */
+public class MutiplexerTimeServer implements Runnable {
+
+    private Selector selector;
+    private ServerSocketChannel serverSocketChannel;
+    private volatile boolean stop;
+
+    public MutiplexerTimeServer(int port) {
+        try {
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(port), 1024);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("the time server is start in port:" + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void stop() {
+        this.stop = true;
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectorKeys = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectorKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handleInput(key);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        key.cancel();
+                        if (key.channel() != null) {
+                            key.channel().close();
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (selector != null) {
+            try {
+                selector.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            if (key.isAcceptable()) {
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                sc.register(selector, SelectionKey.OP_READ);
+            }
+            if (key.isReadable()) {
+                SocketChannel sc = (SocketChannel) key.channel();
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("The time server receive order: " + body);
+                    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+                    doWriter(sc, currentTime);
+                } else if (readBytes < 0) {
+                    key.cancel();
+                    sc.close();
+                } else {
+                    //
+                }
+            }
+        }
+    }
+
+    private void doWriter(SocketChannel sc, String response) throws IOException {
+        if (response != null && response.trim().length() > 0) {
+            byte[] bytes = response.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            writeBuffer.flip();
+            sc.write(writeBuffer);
+        }
+    }
+}
---
+++ java_conclusion/src/main/java/zhwb/study/nio/TImeServer.java
@@ -0,0 +1,22 @@
+package zhwb.study.nio;
+
+/**
+ * Date: 14-6-30
+ * Time: 下午9:40
+ *
+ * @author jack.zhang
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        MutiplexerTimeServer server = new MutiplexerTimeServer(port);
+        new Thread(server,"NIO-Server-001").start();
+    }
+}
