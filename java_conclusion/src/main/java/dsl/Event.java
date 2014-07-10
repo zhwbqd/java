daff13255e4bb6230d53d2c070a5da57a3a4f2a7
zhwbqd <zhwbqd@gmail.com>
2014-07-10 22:52:41 +0800

    add dsl

---
+++ java_conclusion/src/main/java/dsl/Event.java
@@ -0,0 +1,13 @@
+package dsl;
+
+/**
+ * Date: 14-7-4
+ * Time: 下午9:50
+ *
+ * @author jack.zhang
+ */
+public class Event extends AbstractEvent{
+    public Event(String name, String code) {
+        super(name, code);
+    }
+}
