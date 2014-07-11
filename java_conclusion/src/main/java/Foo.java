78fca7e247e139c7195fea4aa369ff5b88afb8aa
jack.zhang <jack.zhang@vipshop.com>
2014-07-11 09:27:03 +0800

    classloader reload

---
+++ java_conclusion/src/main/java/Foo.java
@@ -0,0 +1,5 @@
+public class Foo{
+    public void sayHello() { 
+        System.out.println("hello world! (version one)");
+    } 
+}
---
+++ java_conclusion/src/main/java/zhwb/study/classloader/hotswap/TimerTask.java
@@ -0,0 +1,40 @@
+package zhwb.study.classloader.hotswap;
+
+import java.lang.reflect.Method;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Date: 2014/7/11
+ * Time: 8:54
+ *
+ * @author jack.zhang
+ */
+public class TimerTask {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        sleep(2 * 1000);
+                        try {
+                            // 每次都创建出一个新的类加载器
+                            HotswapCL cl = new HotswapCL("target/classes/swap", new String[]{"Foo"});
+                            Class cls = cl.loadClass("Foo");
+                            Object foo = cls.newInstance();
+
+                            Method m = foo.getClass().getMethod("sayHello", new Class[]{});
+                            m.invoke(foo, new Object[]{});
+
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+}
