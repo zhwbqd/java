e613a540634f45ee92d0db00cd1dd69f16049db6
jack.zhang <jack.zhang@vipshop.com>
2014-10-20 18:01:26 +0800

    and test and

---
+++ drools/drools_service/src/main/java/drools/study/TestAnd.java
@@ -0,0 +1,36 @@
+package drools.study;
+
+import org.drools.KnowledgeBase;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.impl.ClassPathResource;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+import java.util.ArrayList;
+
+/**
+ * @author jack.zhang
+ * @since 2014/10/20
+ */
+public class TestAnd {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+        KnowledgeBuilder
+                kb = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kb.add(new ClassPathResource("test/test_and.drl"),
+                ResourceType.DRL);
+        KnowledgeBase knowledgeBase = kb.newKnowledgeBase();
+
+        StatefulKnowledgeSession statefulKnowledgeSession = knowledgeBase.newStatefulKnowledgeSession();
+
+        From f = new From();
+        f.setNums(new ArrayList(){{
+            add(1);
+        }});
+        f.setIps("1");
+        statefulKnowledgeSession.insert(f);
+
+        statefulKnowledgeSession.fireAllRules();
+        statefulKnowledgeSession.dispose();
+    }
+}
